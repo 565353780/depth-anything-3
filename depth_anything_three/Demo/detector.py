9df1c4cf08f392553119edf094e17090fc581aad
chLi <565353780@qq.com>
2026-01-05 18:15:27 +0800

    set correct extrinsic for camera

--- depth_anything_three/Demo/detector.py
+++ depth_anything_three/Demo/detector.py
@@ -1,4 +1,9 @@
+import sys
+sys.path.append('../camera-control')
+
 import os
+import numpy as np
+import open3d as o3d
 
 from depth_anything_three.Module.detector import Detector
 
@@ -11,5 +16,11 @@ def demo():
     use_ray_pose = False
 
     detector = Detector(model_folder_path, device)
+
     prediction = detector.detectRenderDataFile(render_data_file_path, use_ray_pose)
+
+    points = detector.visPrediction(prediction)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points.cpu().numpy().astype(np.float64))
+    o3d.io.write_point_cloud(home + "/chLi/Dataset/MM/Match/nezha/da3/render_data.ply", pcd)
     return True
