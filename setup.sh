ebd1dd1eb2018c42df66009142deeaf937577172
chLi <565353780@qq.com>
2026-04-02 16:56:10 +0800

    simplify setup bash files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
-cd ..
-git clone https://github.com/ByteDance-Seed/Depth-Anything-3.git da3
-#git clone https://github.com/nerfstudio-project/gsplat.git
-#git clone --recursive https://github.com/facebookresearch/xformers.git
-
 pip install ninja pillow
 
 pip install gradio==5.50.0
@@ -10,16 +5,8 @@ pip install gradio==5.50.0
 pip3 install torch torchvision \
   --index-url https://download.pytorch.org/whl/cu124
 
-cd xformers
-#git checkout v0.0.33
-#python setup.py install
-
 pip3 install -U xformers \
   --index-url https://download.pytorch.org/whl/cu124
 
 pip install --no-build-isolation \
   git+https://github.com/nerfstudio-project/gsplat.git@0b4dddf04cb687367602c01196913cde6a743d70
-
-cd ../da3
-git pull
-pip install -e .
